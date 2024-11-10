d82083732f141cfaf1b6ddcce4cb46040e007a27
dev-yeon <nochi@kakao.com>
2024-11-11 00:12:37 +0900

    [server]server action분리,
    action 진행상황과
    그에 따른 메세지로 ui 변경
    (password)

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,16 +1,15 @@
+'use client';
 import FormInput from '@/components/form-input';
 import FormButton from '@/components/form-btn';
 import SocialLogin from '@/components/social-login';
+import { redirect } from 'next/navigation';
+import { useFormState } from 'react-dom';
+import { error } from 'console';
+import { useActionState } from 'react';
+import { handleForm } from './actions';
 
 export default function Login() {
-    async function handleForm(formData: FormData) {
-        'use server';
-        await new Promise((resolve) => setTimeout(resolve, 5000));
-        console.log('logged in!');
-
-        console.log(formData.get('email'), formData.get('password'));
-        console.log('I run in the server baby!');
-    }
+    const [state, action] = useActionState(handleForm,  null);
 
     return (
         <div className="flex flex-col gap-10 py-8 px-6">
@@ -18,9 +17,20 @@ export default function Login() {
                 <h1 className="text-2xl">안녕하세요!</h1>
                 <h2 className="text-xl">Log in with email and password</h2>
             </div>
-            <form action={handleForm} className="flex flex-col gap-3">
-                <FormInput name="email" type="email" placeholder="Email" required errors={[]} />
-                <FormInput name="password" type="password" placeholder="Password" required errors={[]} />
+            <form action={action} 
+                className="flex flex-col gap-3">
+                <FormInput 
+                    name="email" 
+                    type="email" 
+                    placeholder="Email" 
+                    required 
+                    errors={[]} />
+                <FormInput 
+                    name="password" 
+                    type="password" 
+                    placeholder="Password" 
+                    required 
+                    errors={state?.errors ?? []} />
                 <FormButton text="Log in" />
             </form>
 
