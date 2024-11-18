1ab9444e7698987c1aa391a09f197b5240be371c
dev-yeon <nochi@kakao.com>
2024-11-19 04:59:12 +0900

    scroll infinite scrolling

--- components/product-list.tsx
+++ components/product-list.tsx
@@ -8,36 +8,58 @@ import { getMoreProducts } from '@/app/(tabs)/products/actions';
 interface ProductListProps {
     initialProducts: InitialProducts;
 }
-
 export default function ProductList({ initialProducts }: ProductListProps) {
     const [products, setProducts] = useState(initialProducts);
     const [isLoading, setIsLoading] = useState(false);
     const [page, setPage] = useState(0);
     const [isLastPage, setIsLastPage] = useState(false);
     const trigger = useRef<HTMLSpanElement>(null); // variable 내부에 data를 저장이 가능한 hook
-    useEffect(() => {}, [page]);
-    const onLoadMoreClick = async () => {
-        setIsLoading(true);
-        const newProducts = await getMoreProducts(page + 1);
-        if (newProducts.length !== 0) {
-            setPage((prev) => prev + 1);
-            setProducts((prev) => [...prev, ...newProducts]);
-        } else {
-            setIsLastPage(true);
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            async (entries: IntersectionObserverEntry[], observer: IntersectionObserver) => {
+                const element = entries[0];
+                if (element.isIntersecting && trigger.current) {
+                    observer.unobserve(trigger.current);
+                    setIsLoading(true);
+                    const newProducts = await getMoreProducts(page + 1);
+                    if (newProducts.length !== 0) {
+                        setPage((prev) => prev + 1);
+                        setProducts((prev) => [...prev, ...newProducts]);
+                    } else {
+                        setIsLastPage(true);
+                    }
+                    setIsLoading(false);
+                }
+            },
+            {
+                threshold: 1.0,
+                rootMargin: '0px 0px -100px 0px'
+            }
+        );
+        if (trigger.current) {
+            observer.observe(trigger.current);
         }
-        setIsLoading(false);
-    };
+        return () => {
+            observer.disconnect();
+        };
+    }, [page]);
+
     return (
         <div className="p-5 flex flex-col gap-10">
             {products.map((product) => (
                 <ListProduct key={product.id} {...product} />
             ))}
-            <span
-                ref={trigger}
-                className="mt-[300vh] mb-96 text-sm font-semibold bg-orange-500 w-fit mx-auto px-3 py-2 rounded-md hover:opacity-90 active:scale-95"
-            >
-                {isLoading ? '로딩 중' : 'Load more'}
-            </span>
+            {!isLastPage ? (
+                <span
+                    ref={trigger}
+                    style={{
+                        marginTop: `${page + 1 * 900}vh`
+                    }}
+                    className=" mb-96 text-sm font-semibold bg-orange-500 w-fit mx-auto px-3 py-2 rounded-md hover:opacity-90 active:scale-95"
+                >
+                    {isLoading ? '로딩 중' : 'Load more'}
+                </span>
+            ) : null}
         </div>
     );
 }
