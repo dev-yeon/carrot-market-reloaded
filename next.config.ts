c56b7478dbc7b2b63ec47faa204fd3d5fe26c483
dev-yeon <nochi@kakao.com>
2024-11-18 23:38:22 +0900

    error fixing...

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,13 @@ const nextConfig: NextConfig = {
     /* config options here */
     eslint: {
         ignoreDuringBuilds: true // 빌드 시 ESLint 오류 무시
+    },
+    images: {
+        remotePatterns: [
+            {
+                hostname: 'avatars.githubusercontent.com'
+            }
+        ]
     }
 };
 
