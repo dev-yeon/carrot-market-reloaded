fda4ac028703f5d6aacbb11ebb9a685c76e25fd5
dev-yeon <nochi@kakao.com>
2024-11-19 03:09:43 +0900

    중간세이브

--- app/(tabs)/products/page.tsx
+++ app/(tabs)/products/page.tsx
@@ -1,7 +1,8 @@
-import ListProduct from '@/components/list-product';
+import ProductList from '@/components/product-list';
 import db from '@/lib/db';
+import { Prisma } from '@prisma/client';
 
-async function getProducts() {
+async function getInitialProducts() {
     const products = await db.product.findMany({
         select: {
             title: true,
@@ -9,18 +10,22 @@ async function getProducts() {
             created_at: true,
             photo: true,
             id: true
+        },
+        take: 1,
+        orderBy: {
+            created_at: 'desc'
         }
     });
     return products;
 }
 
+export type InitialProducts = Prisma.PromiseReturnType<typeof getInitialProducts>;
+
 export default async function Products() {
-    const products = await getProducts();
+    const initialProducts = await getInitialProducts();
     return (
         <div>
-            {products.map((product) => (
-                <ListProduct key={product.id} {...product} />
-            ))}
+            <ProductList initialProducts={initialProducts} />
         </div>
     );
 }
