37083c386363165b99549972fd309022a43122bf
dev-yeon <nochi@kakao.com>
2024-11-27 05:24:13 +0900

    chat
    db migration for chat

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,85 +1,172 @@
-import db from '@/lib/db';
-import getSession from '@/lib/session';
-import { formatToWon } from '@/lib/utils';
-import { UserIcon } from '@heroicons/react/24/solid';
-import Image from 'next/image';
-import Link from 'next/link';
-import { notFound } from 'next/navigation';
+import db from "@/lib/db";
+import { formatToWon } from "@/lib/utils";
+import { UserIcon } from "@heroicons/react/24/solid";
+import Image from "next/image";
+import { notFound, redirect } from "next/navigation";
+import {
+  unstable_cache as nextCache,
+  revalidatePath,
+  revalidateTag,
+} from "next/cache";
+import getSession from "@/lib/session";
 
 async function getIsOwner(userId: number) {
-    const session = await getSession();
-    if (session.id) {
-        return session.id === userId;
-    }
-    return false;
+  // const session = await getSession();
+  // if (session.id) {
+  //   return session.id === userId;
+  // }
+  return false;
 }
 
 async function getProduct(id: number) {
-    const product = await db.product.findUnique({
-        where: {
-            id
+  const product = await db.product.findUnique({
+    where: {
+      id,
+    },
+    include: {
+      user: {
+        select: {
+          username: true,
+          avatar: true,
         },
-        include: {
-            user: {
-                select: {
-                    username: true,
-                    avatar: true
-                }
-            }
-        }
-    });
-    return product;
+      },
+    },
+  });
+  
+  return product;
 }
 
-export default async function ProductDetail({ params }: { params: { id: string } }) {
-    const { id: idParam } = await params; // 비동기적으로 params.id를 가져온다.
+const getCachedProduct = nextCache(getProduct, ["product-detail"], {
+  tags: ["product-detail"],
+});
 
-    const id = Number(idParam);
+async function getProductTitle(id: number) {
+  const product = await db.product.findUnique({
+    where: {
+      id,
+    },
+    select: {
+      title: true,
+    },
+  });
+  return product;
+}
+
+const getCachedProductTitle = nextCache(getProductTitle, ["product-title"], {
+  tags: ["product-title"],
+});
+
+export async function generateMetadata({ params }: { params: Promise<{ id: string }> }) {
+    const resolvedParams = await params;
+    const product = await getCachedProductTitle(Number(resolvedParams.id));
+    
+    return {
+      title: product?.title,
+    };
+  }
+
+  export default async function ProductDetail({
+    params,
+  }: {
+    params: Promise<{ id: string }>;
+  }) {
+    const resolvedParams = await params;
+    const id = Number(resolvedParams.id);
     if (isNaN(id)) {
-        return notFound();
+      return notFound();
     }
-
-    const product = await getProduct(id).catch(() => null);
+    const product = await getCachedProduct(id);
     if (!product) {
-        return notFound();
+      return notFound();
     }
-
-    const isOwner = await getIsOwner(product.userId).catch(() => false);
-    return (
-        <div>
-            <div className="relative aspect-square">
-                <Image className="object-cover" fill src={`${product.photo}/public`} alt={product.title} />
-            </div>
-            <div className="p-5 flex items-center gap-3 border-b border-neutral-700">
-                <div className="size-10 overflow-hidden rounded-full">
-                    {product.user.avatar !== null ? (
-                        <Image src={product.user.avatar} width={40} height={40} alt={product.user.username} />
-                    ) : (
-                        <UserIcon />
-                    )}
-                </div>
-                <div>
-                    <h3>{product.user.username}</h3>
-                </div>
-            </div>
-            <div className="p-5">
-                <h1 className="text-2xl font-semibold">{product.title}</h1>
-                <p>{product.description}</p>
-            </div>
-            <div className="fixed w-full bottom-0 left-0 p-5 pb-10 bg-neutral-800 flex justify-between items-center">
-                <span className="font-semibold text-xl">{formatToWon(product.price)}원</span>
-                {isOwner ? (
-                    <button className="bg-red-500 px-5 py-2.5 rounded-md text-white font-semibold">
-                        Delete product
-                    </button>
-                ) : null}
-                <form  >
-                    <button className="bg-orange-500 px-5 py-2.5 rounded-md text-white font-semibold">
-                        채팅하기
-                    </button>
-                </form>
-       
-            </div>
+      // 여기에서 이미지 URL을 출력합니다.
+  console.log('Product photo URL:', product.photo);
+  
+  const isOwner = await getIsOwner(product.userId);
+  const revalidate = async () => {
+    "use server";
+    revalidateTag("xxxx");
+  };
+  const createChatRoom = async () => {
+    "use server";
+    const session = await getSession();
+    const room = await db.chatRoom.create({
+      data: {
+        users: {
+          connect: [
+            {
+              id: product.userId, // 판매자
+            },
+            {
+              id: session.id, // 구매자 //session은 로그인 한 사람 
+            },
+          ],
+        },
+      },
+      select: {
+        id: true,
+      },
+    });
+    redirect(`/chats/${room.id}`); // 채팅방으로 이동 
+  };
+  return (
+    <div className="pb-40">
+      <div className="relative aspect-square">
+        <Image
+          className="object-cover"
+          fill
+          src={`${product.photo}/public`}
+          alt={product.title}
+        />
+      </div>
+      <div className="p-5 flex items-center gap-3 border-b border-neutral-700">
+        <div className="size-10 overflow-hidden rounded-full">
+          {product.user.avatar !== null ? (
+            <Image
+              src={product.user.avatar}
+              width={40}
+              height={40}
+              alt={product.user.username}
+            />
+          ) : (
+            <UserIcon />
+          )}
         </div>
-    );
+        <div>
+          <h3>{product.user.username}</h3>
+        </div>
+      </div>
+      <div className="p-5">
+        <h1 className="text-2xl font-semibold">{product.title}</h1>
+        <p>{product.description}</p>
+      </div>
+      <div className="fixed w-full bottom-0  p-5 pb-10 bg-neutral-800 flex justify-between items-center max-w-screen-sm">
+        <span className="font-semibold text-xl">
+          {formatToWon(product.price)}원
+        </span>
+        {isOwner ? (
+          <form action={revalidate}>
+            <button className="bg-red-500 px-5 py-2.5 rounded-md text-white font-semibold">
+              Revalidate title cache
+            </button>
+          </form>
+        ) : null}
+        <form action={createChatRoom}>
+          <button className="bg-orange-500 px-5 py-2.5 rounded-md text-white font-semibold">
+            채팅하기
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export async function generateStaticParams() {
+  const products = await db.product.findMany({
+    select: {
+      id: true,
+    },
+  });
+  return products.map((product) => ({ id: product.id + "" }));
+  
 }
